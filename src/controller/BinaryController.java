d607b46b12730bd3a46870c3026310a1ac24a14a
Tami Oliveira <tamiresaldeoliveira@gmail.com>
2025-02-23 23:16:54 -0300

    comentários

--- src/controller/BinaryController.java
+++ src/controller/BinaryController.java
@@ -2,9 +2,14 @@ package controller;
 
 public class BinaryController {
 	public String convbinary(int decimal) {
+		// o ponto de parada se dá quando não for possível dividir o decimal por 2
 		if (decimal == 0) {
 			return "";
 		}
+		
+		/* a função é chamada passando o decimal dividindo por 2, e é feita a concatenação 
+		do resto dessa divisão convertido em string */
+		
 		return(convbinary(decimal / 2) + String.valueOf(decimal % 2));
 		
 			
